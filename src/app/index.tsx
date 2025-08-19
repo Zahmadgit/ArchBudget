e9387af094ba3c160812fd3e2e46202461dcc0bd
Zeshan Ahmad <zahmad5870@gmail.com>
2025-08-19 15:31:10 -0400

    init

---
+++ src/app/index.tsx
@@ -0,0 +1,7 @@
+import { WelcomeScreen } from "@/screens/WelcomeScreen"
+
+// import { WelcomeScreenZ } from "@/screens/WelcomeScreenZ"
+
+export default function Index() {
+  return <WelcomeScreen />
+}
